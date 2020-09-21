7933f31d5d9eba0e0499a3f2bbc7d35375c39352
nick zervos <zaknafien99@hotmail.com>
2020-09-21 14:28:44 +1000

    Add project files.

---
+++ CryptTesting/MainPage.xaml.cpp
@@ -0,0 +1,40 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace CryptTesting;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+
+void CryptTesting::MainPage::encrypt_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	TextBox plainText;
+	TextBox cipherText;
+		
+	Button^ btn = (Button^)sender;	//cast the sender handle to a button
+	
+	// take the text from the plain text field, encrypt it and display the ciphered text in the ciphertext field.
+	this->cipherText->Text = this->plainText->Text; //copies the text from one field to the other
+	
+}
